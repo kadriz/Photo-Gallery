02bb90a243b351b221ada89059f0294309becad6
kadriz <onlinekz2021@gmail.com>
2023-05-21 22:33:11 +0200

    Update Form.jsx

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -45,9 +45,9 @@ async function magicLinkLogin() {
   const { data } = await supabase.auth.signInWithOtp({
     email: mail ,
  })
-}
   setAlert ('Check your email for a Supabase Magic Link to log in!','succes')
   setMail('')
+}  
 }   
 async function signOut() {
   const { error } = await supabase.auth.signOut()
@@ -104,7 +104,7 @@ async function signInWithGoogle() {
         
           <div >
               <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email</label>
-              <input  onChange={(e)=>setMail(e.target.value)} value={mail ||""} type="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@flowbite.com"/>
+              <input  onChange={(e)=>setMail(e.target.value)} value={mail ||""} type="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Enter your email address"/>
               <div className='flex justify-center '>
          <button onClick={(e)=>(magicLinkLogin(), e.preventDefault())} className="btn btn-outline my-3 py-3 px-5 text-sm font-medium text-center rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"> Get Magic Link</button>
           </div>
