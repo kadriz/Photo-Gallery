0ecaaa4cbaf15d4195da599d3483d2855b29589a
kadriz <onlinekz2021@gmail.com>
2023-05-22 23:01:37 +0200

    Update Form.jsx

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -100,14 +100,13 @@ async function signInWithGoogle() {
        
        <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Enter an email to sign in with Supabase Magic Link</p>
         <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
-        
+             <Alert/> 
           <div >
               <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email</label>
               <input  onChange={(e)=>setMail(e.target.value)} value={mail ||""} type="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Enter your email address"/>
               <div className='flex justify-center '>
          <button onClick={(e)=>(magicLinkLogin(), e.preventDefault())} className="btn btn-outline my-3 py-3 px-5 text-sm font-medium text-center rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"> Get Magic Link</button>
           </div>
-           <Alert/> 
            </div>
           <div className="relative flex items-center justify-center w-full mt-6 border border-t">
                     <div className="absolute px-5 bg-white">Or</div>
